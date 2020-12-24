abea1de5e13cad7f03581afe394a382daf8e791a
Cyy137 <1374608795@qq.com>
2020-12-24 11:46:23 +0800

    第一个类

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+   public class Enemy
+    {
+        public void Move()
+        {
+            Console.WriteLine("调用了 enemy的move方法");
+        }
+        public virtual void Attack()
+        {
+            Console.WriteLine("enemy attac");
+        }
+
+      
+    }
+}
